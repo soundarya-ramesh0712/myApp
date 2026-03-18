4db401e7d9e79a8068a61b0b1a34e99a402caf3e
soundarya-ramesh0712 <soundaryar0712@gmail.com>
2026-03-18 10:52:30 +0530

    update navbar styles and hover effects

--- src/app/app.ts
+++ src/app/app.ts
@@ -4,7 +4,17 @@ import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-root',
   // templateUrl: './app.html',
-  template:`<h1>Hello world</h1>`,
+  template:`
+  <nav>
+    <a href="/">Home</a>
+    <a href="/about">About</a>
+    <a href="/contact">contact</a>
+  </nav>
+  <router-outlet></router-outlet>
+  <h1>Hello world</h1>
+  <p>Welcome to my angular app</p>
+  <p>Currently I am doing intership at <b>"Black I technologies and solutions"</b></p>
+  <p>I am learning Angular</p>`,
   styleUrls:['./app.scss'],
   // styleUrl: './app.scss'
   imports: [RouterOutlet],
